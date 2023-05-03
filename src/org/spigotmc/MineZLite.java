cfab85c3bb8b442858365ba33a56d644fe96c006
Jordan Samuel <jtsamuel@willamette.edu>
2023-05-03 16:19:03 -0700

    Added a broadcast command for players

--- src/org/spigotmc/MineZLite.java
+++ src/org/spigotmc/MineZLite.java
@@ -1,15 +1,36 @@
 package org.spigotmc;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 
 public class MineZLite extends JavaPlugin {
 
     @Override
     public void onEnable() {
-         getLogger().info("Hello, World! MineZLite has been enabled.");
+        getLogger().info("Hello, World! MineZLite has been enabled.");
     }
 
     @Override
     public void onDisable() {
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("broadcast")) {
+            if (args.length > 0) {
+                StringBuilder message = new StringBuilder();
+                for (String arg : args) {
+                    message.append(arg).append(" ");
+                }
+                getServer().broadcastMessage(ChatColor.GREEN + sender.getName() + " says: " + ChatColor.WHITE + message.toString().trim());
+                return true;
+            } else {
+                sender.sendMessage(ChatColor.RED + "Usage: /broadcast <message>");
+                return false;
+            }
+        }
+        return false;
     }
 }
