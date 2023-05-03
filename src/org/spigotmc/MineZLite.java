20402876ec65610b526c2706bca5bd74ec41c0c4
Jordan Samuel <jtsamuel@willamette.edu>
2023-05-03 10:10:42 -0700

    Initial build

---
+++ src/org/spigotmc/MineZLite.java
@@ -0,0 +1,14 @@
+package org.spigotmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MineZLite extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
